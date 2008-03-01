5103ac75127b1d8b744870da3df4a602f8145e47
Tim Carey-Smith <tim@spork.in>
2008-03-01 13:49:18 +1300

    Use git config --get for obtaining the single config value

--- commands.rb
+++ commands.rb
@@ -1,5 +1,5 @@
 GitHub.register :open do 
-  if remote = `git config -l`.split("\n").detect { |line| line =~ /remote.origin.url/ }
+  if remote = `git config --get remote.origin.url`.chomp
     exec "open http://github.com/#{remote.split(':').last.chomp('.git')}"
   end
 end

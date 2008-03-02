5a4afe762223bdd63b5f9424a1433b718d5be28e
Chris Wanstrath <chris@ozmm.org>
2008-03-01 17:55:38 -0800

    super basic descriptions

--- commands.rb
+++ commands.rb
@@ -3,18 +3,21 @@ GitHub.register :helper do |name, comma_args|
   puts helper.send(name, comma_args.split(/,/))
 end
 
+GitHub.describe :home => "Open this repo's master branch in a web browser."
 GitHub.register :home do
   if helper.project
     exec "open #{helper.homepage_for(helper.owner, 'master')}"
   end
 end
 
+GitHub.describe :browse => "Open this repo in a web browser."
 GitHub.register :browse do
   if helper.project
     exec "open #{helper.homepage_for(helper.branch_user, helper.branch_name)}"
   end
 end
 
+GitHub.describe :info => "Info about this project."
 GitHub.register :info do
   puts "== Info for #{helper.project}"
   puts "You are #{helper.owner}"
@@ -24,6 +27,7 @@ GitHub.register :info do
   end
 end
 
+GitHub.describe :track => "Track another user's repository."
 GitHub.register :track do |user|
   die "Specify a user to track" if user.nil?
   die "Already tracking #{user}" if helper.tracking?(user)
@@ -31,7 +35,7 @@ GitHub.register :track do |user|
   git "remote add #{user} #{helper.public_url_for(user)}"
 end
 
-GitHub.describe :pull => 'hi, this is github pull'
+GitHub.describe :pull => 'Pull from a remote.'
 GitHub.register :pull do |user, branch|
   die "Specify a user to pull from" if user.nil?
   GitHub.invoke(:track, user) unless helper.tracking?(user)

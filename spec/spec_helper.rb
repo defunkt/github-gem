982e91567b699bf18910293123bb16b95238e7c8
Kevin Ballard <kevin@sb.org>
2008-05-14 03:23:33 -0500

    Also guard Kernel#system

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,22 +14,27 @@ Spec::Runner.configure do |configuration|
   end
 
   backtick = nil # establish the variable in this scope
+  saved_system = nil
   configuration.prepend_before(:all) do
-    # raise an exception if the `` operator is used
+    # raise an exception if Kernel#` or Kernel#system is used
     # in our tests, we want to ensure we're fully self-contained
     Kernel.instance_eval do
       backtick = instance_method(:`)
-      alias_method(:_backtick, :`)
+      saved_system = instance_method(:system)
       define_method :` do |str|
         raise "Cannot use backticks in tests"
+      end
+      define_method :system do |*args|
+        raise "Cannot use Kernel#system in tests"
       end
     end
   end
 
   configuration.prepend_after(:all) do
-    # and now restore the `` operator
+    # and now restore Kernel#` and Kernel#system
     Kernel.instance_eval do
       define_method :`, backtick
+      define_method :system, saved_system
     end
   end
 end

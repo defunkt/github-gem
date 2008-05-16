304a88881a2b01ba55ec43945ba343bb0cd56a44
Kevin Ballard <kevin@sb.org>
2008-05-16 00:52:01 -0400

    Guard Process.fork and Open3.popen3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,21 +4,30 @@ require 'spec'
 require File.dirname(__FILE__) + '/../lib/github'
 
 class Spec::Example::Configuration
-  def add_guard(klass, name)
+  def add_guard(klass, name, isclass = false)
     guarded = nil
+    target = (isclass ? (class << klass;self;end) : klass)
     self.prepend_before(:all) do
-      klass.instance_eval do
+      target.class_eval do
         guarded = instance_method(name)
         define_method name do |*args|
-          raise "Testing guards violated: Cannot call #{klass}##{name}"
+          raise "Testing guards violated: Cannot call #{klass}#{isclass ? "." : "#"}#{name}"
         end
       end
     end
     self.prepend_after(:all) do
-      klass.instance_eval do
-        define_method name, guarded
+      target.class_eval do
+        if guarded.nil?
+          undef_method name
+        else
+          define_method name, guarded
+        end
       end
     end
+  end
+
+  def add_class_guard(klass, name)
+    add_guard(klass, name, true)
   end
 end
 
@@ -34,4 +43,6 @@ Spec::Runner.configure do |configuration|
 
   configuration.add_guard(Kernel, :`)
   configuration.add_guard(Kernel, :system)
+  configuration.add_class_guard(Process, :fork)
+  configuration.add_class_guard(Open3, :popen3) if defined? Open3
 end

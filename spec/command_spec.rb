f88b01ca7fce375234e5cc8d37fcbce2cafc2daf
Kevin Ballard <kevin@sb.org>
2008-05-16 02:32:57 -0400

    Unguard Open3.popen3 when we're testing the GitHub::Command#sh method

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -20,6 +20,7 @@ describe GitHub::Command do
   end
 
   it "should successfully call out to the shell" do
+    class_unguard(Open3, :popen3)
     hi = @command.sh("echo hi")
     hi.should == "hi"
     hi.out?.should be(true)

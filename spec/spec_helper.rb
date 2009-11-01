24c1fd0bdc64672a825792a430299d682a442317
Dr Nic Williams <drnicwilliams@gmail.com>
2009-11-02 09:56:58 +1000

    Fix the rp/rputs test for TextMate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -140,7 +140,7 @@ end
 # When running specs in TextMate, provide an rputs method to cleanly print objects into HTML display
 # From http://talklikeaduck.denhaven2.com/2009/09/23/rspec-textmate-pro-tip
 module Kernel
-  if ENV.keys.find {|env_var| env_var.index("TM_") >= 0}
+  if ENV.keys.find {|env_var| env_var.index("TM_")}
     def rputs(*args)
       require 'cgi'
       puts( *["<pre>", args.collect {|a| CGI.escapeHTML(a.to_s)}, "</pre>"])

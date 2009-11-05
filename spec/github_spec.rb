deb53e3efcb18678de45be2013214dd60bcb6703
Dr Nic Williams <drnicwilliams@gmail.com>
2009-11-05 15:08:29 +1000

    Ensure spec_helper.rb is only loaded once

--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "GitHub.parse_options" do
   it "should parse --bare options" do

b5166fce5364265db530aec40e3268762674c537
Dr Nic Williams <drnicwilliams@gmail.com>
2009-11-06 09:54:37 +1000

    specs for 'issues open|closed'

--- spec/commands/command_issues_spec.rb
+++ spec/commands/command_issues_spec.rb
@@ -21,6 +21,39 @@ describe "github issues" do
   end
     
   specify "issues web opens the project's issues page" do
+    running :issues, "open" do
+      setup_url_for
+      mock_issues_for "open"
+      stdout.should == <<-EOS.gsub(/^      /, '')
+      -----
+      Issue #1 (0 votes): members.json 500 error
+      *  Opened about 19 hours ago by kdmny
+      *  Last updated about 18 hours ago
+      
+      I have a nasty bug.
+      -----
+      EOS
+    end
+  end
+
+  specify "issues web closed the project's issues page" do
+    running :issues, "closed" do
+      setup_url_for
+      mock_issues_for "closed"
+      stdout.should == <<-EOS.gsub(/^      /, '')
+      -----
+      Issue #1 (0 votes): members.json 500 error
+      *  Opened about 19 hours ago by kdmny
+      *  Closed about 18 hours ago
+      *  Last updated about 18 hours ago
+      
+      I have a nasty bug.
+      -----
+      EOS
+    end
+  end
+
+  specify "issues web opens the project's issues page" do
     running :issues, "web" do
       setup_url_for
       @helper.should_receive(:open).once.with("https://github.com/user/project/issues")
@@ -31,6 +64,29 @@ describe "github issues" do
     running :issues, "web", "drnic" do
       setup_url_for
       @helper.should_receive(:open).once.with("https://github.com/drnic/project/issues")
+    end
+  end
+  
+  class CommandHelper::Runner
+    def mock_issues_for(state = "open", user = "user", project = "project")
+      yaml = <<-YAML.gsub(/^    /, '')
+      --- 
+      issues: 
+      - number: 1
+        votes: 0
+        created_at: 2009-11-04 20:25:02 -08:00
+        body: |-
+          I have a nasty bug.
+        title: members.json 500 error
+        updated_at: 2009-11-04 21:26:39 -08:00
+        #{"closed_at: 2009-11-04 21:25:17 -08:00" if state == "closed"}
+        user: kdmny
+        labels: []
+
+        state: #{state}
+      YAML
+      api_url = "http://github.com/api/v2/yaml/issues/list/#{user}/#{project}/#{state}"
+      @command.should_receive(:open).with(api_url).and_return(yaml)
     end
   end
 end

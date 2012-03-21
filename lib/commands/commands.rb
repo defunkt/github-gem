fe192b18f44e25aae411cd2b68235ee476e21876
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-20 19:30:59 -0700

    fix the create-from-local; it returns json so look for ['error']

--- lib/commands/commands.rb
+++ lib/commands/commands.rb
@@ -249,13 +249,13 @@ command :'create-from-local' do |repo_name|
   is_repo = !git("status").match(/fatal/)
   raise "Not a git repository. Use 'gh create' instead" unless is_repo
   command = "curl -F 'name=#{repo}' -F 'public=#{options[:private] ? 0 : 1}' -F 'login=#{github_user}' -F 'token=#{github_token}' https://github.com/api/v2/json/repos/create"
-  created = sh command
-  if created.out =~ %r{You are being <a href="https://github.com/#{github_user}/([^"]+)"}
+  output_json = sh command
+  output = JSON.parse(output_json)
+  if output["error"]
+    die output["error"]
+  else
     git "remote add origin git@github.com:#{github_user}/#{repo}.git"
     git_exec "push origin master"
-  else
-    error = JSON.parse(created)
-    die error["error"]
   end
 end
 
